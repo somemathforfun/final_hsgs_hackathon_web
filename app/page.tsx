196968b08e11980b7d532d5e8cf39d20784c5d7b
anHiep <hie2pbrizzily@gmail.com>
2024-06-26 23:07:28 +0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     return (
         <div className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-200 via-white to-white dark:from-zinc-800 dark:via-black dark:to-black">
             <Head>
-                <title>Virtual Staging AI</title>
+                <title>HSGS Hackathon 2024</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
