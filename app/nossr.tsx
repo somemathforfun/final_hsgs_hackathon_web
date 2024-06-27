70e6b922fd5e7cb723f0f44fe2edfe26e5380baf
anHiep <hie2pbrizzily@gmail.com>
2024-06-27 09:22:53 +0700

    changes

--- app/nossr.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const DynamicComponentWithNoSSR = dynamic(() => import('../components/List'), {
-  ssr: false
-})
-
-export default () => <DynamicComponentWithNoSSR />
